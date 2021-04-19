e843c274a8bfa2a6b1bc817dbd03ae9be706e66c
ishitarastogi <ishita3099@gmail.com>
2021-04-19 17:13:07 +0530

    Build ERC20 Token

---
+++ contracts/Token.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor(uint256 initialSupply) public ERC20("Token", "TKN") {
+      
+    }
+}
